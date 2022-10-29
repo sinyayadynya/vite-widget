573d5671e8114bce890a0eb4160739bc20fcd08f
Sinyaya Dynya <sinyayadynya@gmail.com>
2022-10-29 19:53:45 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-widget/'
+  base: '/vite-widget/',
   build: {
     outDir: 'build',
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
+        assetFileNames: assetInfo => {
           let extType = assetInfo.name.split('.').at(1)
           if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
             extType = 'img'
